26273abfbac6963267496be342e105a92394ae6d
Nicholas Cannon <nicholascannon1@gmail.com>
2018-11-13 20:35:27 +0800

    Installed frontend stuff and added react component files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import MelloNav from './components/nav';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * Event methods 
+   */
+  addList(e) {
+    return;
+  }
+
+  deleteList(id) {
+    return;
+  }
+
+  addTask(e) {
+    return;
+  }
+
+  deleteTask(id) {
+    return;
+  }
+
+  logout() {
+    return;
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Mello</h1>
+        <MelloNav />
       </div>
     );
   }
