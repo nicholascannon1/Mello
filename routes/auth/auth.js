f0989fff70eec41463aca36efaef01dbc7287286
Nicholas Cannon <nicholascannon1@gmail.com>
2018-11-26 09:08:35 -0800

    Documentation update

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 const User = require('../../models/User');
 
 /**
- * Login URL. /auth/google/
+ * Login URL: /auth/google/
  * 
  * Sends the user to googles login auth service. This redirect 
  * prompts the user to allow access.
@@ -22,8 +22,7 @@ router.get('/', passport.authenticate('google',{
  * 
  * Google sends the user back to this URL with an auth code.
  * If authentication succeeds, primary route function will be 
- * called. In this case we search the DB for the user matching the 
- * google id given to us by google and generate a JWT for that user.
+ * called. In this case we generate a JWT for the user logging in.
  */
 router.get('/callback', 
   passport.authenticate('google', { session: false }),
