d1f15e3b917f9d57aa6bee7ae4a86645c50e1c8d
Nicholas Cannon <nicholascannon1@gmail.com>
2019-01-06 19:48:39 +0800

    Changed tasks to buttons

--- client/src/components/task.jsx
+++ client/src/components/task.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Task extends React.Component {
   render() {
     return (
-      <li className="list-group-item">{this.props.task}</li>
+      <button type="button" className="list-group-item list-group-item-action">
+        {this.props.task}
+      </button>
     );
   }
 }
